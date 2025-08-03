4a7f2387cea399519433d5432af9a548ae79d7e2
itworkmng <itworkllcompany@gmail.com>
2025-08-03 18:14:53 +0700

    ⚡️ Update fix

--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -12,7 +12,7 @@ import cv from '../../cv/cv.pdf';
 import { FormattedMessage } from 'react-intl';
 
 /* Img */
-import imgabout from '../../img/home.jpg';
+import imgabout from '../../img/home.webp';
 
 const About = () => {
 
@@ -53,7 +53,7 @@ const About = () => {
               <p>
                 <FormattedMessage
                   id='about-info-1'
-                  defaultMessage="Hi, I'm Esteban Nahuel Carrizo, a student at the Technological University of Tucumán; I consider myself a person passionate about programming and web development since in 2020 I had the opportunity to get to know this world of the front-end and I was able to create my first web page only using HTML and CSS from there, I felt a great passion for web development, since you can do amazing things knowing how to use HTML, CSS, JavaScript and to this day I still feel that same passion when I create a web page."
+                  defaultMessage="Бид Next.js, React, Node.js, MySQL, PostgreSQL зэрэг орчин үеийн технологи ашиглан хурдан, найдвартай, хэрэглэгчдэд ээлтэй системүүдийг хөгжүүлдэг. Мөн хэрэглэгчдийн шаардлагад нийцүүлэн захиалгат вэбсайт, QR кодын цэс, автомат тайлан, өгөгдлийн удирдлагын системүүдийг бүтээх туршлагатай."
                 />
               </p>
 
@@ -61,14 +61,14 @@ const About = () => {
                 <p>
                   <FormattedMessage
                     id='about-info-2'
-                    defaultMessage='I consider myself a self-taught person since I like to be constantly learning day by day, both new technologies and new development methods that help me polish and raise my level of learning.'
+                    defaultMessage='Хэрэглэгчиддээ чанар, хурд, аюулгүй байдал гурвыг зэрэг хангахыг зорьж, систем бүр дээрээ SSL, эрхийн хяналт, мэдээллийн хамгаалалтыг нэвтрүүлдэг. Бидний амжилттай хэрэгжүүлсэн төслүүдэд qr-uvs.mn, menu.uvs100.mn, eblank.mn, precilab.mn зэрэг системүүд багтдаг.'
                   />
                 </p>
 
                 <p>
                   <FormattedMessage
                     id='about-info-3'
-                    defaultMessage="I have experience working as a freelance web designer and developer, which gave me the opportunity to work on many interesting projects, adapting to the client's needs and budget, which allowed me to improve my skills and knowledge; Additionally, I have also had the opportunity to be part of some online and face-to-face courses that helped me enrich my skills and learn a little more about this beautiful world of web development."
+                    defaultMessage="IT WORK бол таны найдвартай дижитал түнш. Бидэнтэй хамтран ажилласнаар та зөвхөн нэг вэб сайт бус, бизнесээ цахимжуулах цогц шийдлийг авах болно."
                   />
                 </p>
               </div>
