c053c32c0c3e971d3f4675321e221955e03feb11
itworkmng <itworkllcompany@gmail.com>
2025-08-03 18:02:07 +0700

    🎨 Asd

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -24,13 +24,13 @@ const Content = () => (
                 <h1 data-aos="fade-up" data-aos-delay="800">
                     <FormattedMessage
                         id='name'
-                        defaultMessage='I am Nahuel Carrizo'
+                        defaultMessage='We are IT Work'
                     />
                 </h1>
                 <p data-aos="fade-up" data-aos-delay="1000">
                     <FormattedMessage
                         id='rol'
-                        defaultMessage='Full-stack developer'
+                        defaultMessage='Your trusted digital partner'
                     />
                 </p>
 
